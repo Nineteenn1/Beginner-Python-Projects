b305211244d7436047049d56c31f29c2c6b242e4
fxxsx <83366051+fxxsx@users.noreply.github.com>
2022-04-19 11:33:47 +0200

    Create Simple Mad Libs.py

---
+++ Simple Mad Libs.py
@@ -0,0 +1,6 @@
+print("Mad Libs")
+print("Our School Cafeteria Has really ADJECTIVE Food, Thinking About It Makes Me VERB. The Food Tastes Like NOUN.")
+Adj = input("Enter an adjective")
+Verb = input("Enter a Verb")
+Noun = input("Enter a Noun")
+print(f"Our School Cafeteria Has really {Adj} Food, Thinking About It Makes Me {Verb}. The Food Tastes Like {Noun}.")
