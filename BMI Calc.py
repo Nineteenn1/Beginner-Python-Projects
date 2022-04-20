17f107ff527a835f433f3f4fb96ecf16cf18806f
fxxsx <83366051+fxxsx@users.noreply.github.com>
2022-04-20 18:40:32 +0200

    Update BMI Calc.py

--- BMI Calc.py
+++ BMI Calc.py
@@ -1,5 +1,7 @@
-print("BMI")
+#This program Calculates BMI
+print("BMI\n Less than 18.5 - Underweight\n 18.5–24.9 - Normal Weight\n 25–29.9 - Overweight\n 30 or more - Obesity")
 Weight = float(input("Enter Your Weight (KG)"))
 Height = float(input("Enter Your Height (CM)"))
 BMI = Weight / (Height/100) **2
 print(f"Your BMI is {BMI}")
+
