4477d11b479a6a106ae2377a16253719a398ee23
fxxsx <83366051+fxxsx@users.noreply.github.com>
2022-05-03 11:07:17 +0200

    Create spamBot.py

---
+++ spamBot.py
@@ -0,0 +1,12 @@
+import pyautogui as spam
+import time
+timeout = time.time() + 30 #Change the 30 here to an other number if you want it to last longer or shorter
+while True:
+    time.sleep(1)
+    test = 0
+    spam.typewrite("Spam Eggs Bacon")
+    spam.press('enter')
+    if test == 30 or time.time() > timeout: # and here
+        break
+    test = test -1
+    
