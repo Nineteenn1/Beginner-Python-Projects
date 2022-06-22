ea116cb86fa9bac04c164707ccbc77313875046b
Borys <nineteen.yt123@gmail.com>
2022-06-22 10:34:49 +0200

    Update spamBot.py

--- spamBot.py
+++ spamBot.py
@@ -1,13 +1,12 @@
 import pyautogui as spam
 import time
-timeout = time.time() + 30 #Change the 30 here to an other number if you want it to last longer or shorter
+timeout = time.time() + 90 #Change the 30 here to an other number if you want it to last longer or shorter
 while True:
-    time.sleep(1)
     test = 0
-    spam.typewrite("Spam Eggs Bacon")
+    spam.typewrite("@everyone")
     spam.press('enter')
-    if test == 30 or time.time() > timeout: # and here
+    if test == 90 or time.time() > timeout: # and here
         break
     test = test -1
-    
+    time.sleep(1)
  
